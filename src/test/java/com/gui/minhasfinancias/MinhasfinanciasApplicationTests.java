200c5eb3060ffd794ef28194547e325189983683
guiweb85@gmail.com <guiweb85@gmail.com>
2023-11-03 16:42:24 -0300

    18. Mapeando a Entidade Usuário

---
+++ src/test/java/com/gui/minhasfinancias/MinhasfinanciasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gui.minhasfinancias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinhasfinanciasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
